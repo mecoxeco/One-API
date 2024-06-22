6731d28fd334b5e5bb086c21999e3c3dcafe9a2c
Pedro Henrique de Abreu Lourenço <118847988+PedroHLourenco@users.noreply.github.com>
2024-06-22 14:21:52 -0300

    app module atualizada

--- API BUSSOLA/one-api/src/app.module.ts
+++ API BUSSOLA/one-api/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ChapterModule } from './chapter/chapter.module';
+import { CharacterModule } from './character/character.module';
+import { CrewModule } from './crew/crew.module';
+import { DevilFruitModule } from './devil-fruit/devil-fruit.module';
+import { EpisodeModule } from './episode/episode.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://0.0.0.0/one-api'),
+    ChapterModule,
+    CharacterModule,
+    CrewModule,
+    DevilFruitModule,
+    EpisodeModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }

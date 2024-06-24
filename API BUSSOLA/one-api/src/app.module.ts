987db16c44a1ea41295c86b06c5fa2a9ec62d094
Sunamun <37673813+mecoxeco@users.noreply.github.com>
2024-06-24 00:00:49 -0300

    ededede

--- API BUSSOLA/one-api/src/app.module.ts
+++ API BUSSOLA/one-api/src/app.module.ts
@@ -23,6 +23,7 @@ import { EpisodeModule } from './episode/episode.module';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
+        useNewUrlParser: false,
         useUnifiedTopology: true,
       }),
       inject: [ConfigService],
